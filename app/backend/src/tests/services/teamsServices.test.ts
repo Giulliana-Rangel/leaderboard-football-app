b15df57dce78fc9b97317ca0ce10d3798ed7c53f
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-03-29 16:57:05 -0300

    fix: up reqs 4 e 5

--- app/backend/src/tests/services/teamsServices.test.ts
+++ app/backend/src/tests/services/teamsServices.test.ts
@@ -1,20 +1,41 @@
-import * as sinon from 'sinon';
-import * as chai from 'chai';
-// @ts-ignore
-import chaiHttp = require('chai-http');
+// import * as sinon from 'sinon';
+// import * as chai from 'chai';
+// // @ts-ignore
+// import chaiHttp = require('chai-http');
 
-import { app } from '../../app';
-import teamsModel from '../../database/models/Teams';
-import TeamsService from '../../database/services/teams.service';
+// import { app } from '../../app';
+// import teamsModel from '../../database/models/Teams';
+// import TeamsService from '../../database/services/teams.service';
 
-import { Response } from 'superagent';
+// import { Response } from 'superagent';
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
-const { expect } = chai;
+// const { expect } = chai;
 
-describe('Seu teste', () => {
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
-  });
-});
+// describe('Team Service', () => {
+//    let chaiHttpResponse: Response;
+
+//   before(async () => {
+//     sinon
+//       .stub(Example, "findOne")
+//       .resolves({
+//         ...<Seu mock>
+//       } as Example);
+//   });
+
+//   after(()=>{
+//     (Example.findOne as sinon.SinonStub).restore();
+//   })
+
+//   it('...', async () => {
+//     chaiHttpResponse = await chai
+//        .request(app)
+//        ...
+
+//     expect(...)
+//   });
+//   it('Seu sub-teste', () => {
+//     expect(false).to.be.eq(true);
+//   });
+// });
