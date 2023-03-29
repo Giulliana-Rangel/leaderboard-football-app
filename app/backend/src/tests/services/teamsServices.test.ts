03a03e9f9cd41a47b57d775666d9ad912c071f37
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-03-29 13:04:11 -0300

    fix: up push

---
+++ app/backend/src/tests/services/teamsServices.test.ts
@@ -0,0 +1,20 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../../app';
+import teamsModel from '../../database/models/Teams';
+import TeamsService from '../../database/services/teams.service';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Seu teste', () => {
+  it('Seu sub-teste', () => {
+    expect(false).to.be.eq(true);
+  });
+});
