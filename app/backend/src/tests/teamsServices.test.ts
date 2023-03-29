60099f070313d38fe7aef21ea0b2cb40e68ebb6b
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-03-29 19:12:55 -0300

    feat: req2, teams Tests, dones

---
+++ app/backend/src/tests/teamsServices.test.ts
@@ -0,0 +1,55 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import { Model } from 'sequelize';
+
+import { Response } from 'superagent';
+
+import Teams from '../database/models/Teams';
+import { teams, team } from './mocks/teams.mock';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Team Service', () => {
+  let chaiHttpResponse: Response;
+
+  beforeEach(() => {
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  })
+
+   it('If endpoins "get/teams" return all', async () => {
+    sinon.stub(Model, "findAll")
+    .resolves(teams as Teams[]);
+
+    chaiHttpResponse = await chai
+      .request(app)
+      .get('/teams');
+      // console.log('test', chaiHttpResponse);
+
+      expect(chaiHttpResponse.status).to.be.eq(200);
+      expect(chaiHttpResponse.body).to.be.an('array');
+      expect(chaiHttpResponse.body).to.be.deep.eq(teams);
+   });
+
+   it('If endpoins "get/team/id" return byId', async () => {
+    sinon.stub(Model, "findByPk")
+    .resolves(team as Teams);
+
+    chaiHttpResponse = await chai
+    .request(app)
+    .get('/teams/1');
+
+    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.body).to.be.deep.eq(team);
+   })
+
+
+});
