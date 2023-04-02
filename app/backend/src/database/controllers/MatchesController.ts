61217ac3dec52c0e31821c3120277b108d0e2ab8
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-04-02 10:17:08 -0300

    fix: req 10 works local but crashed in git, up

--- app/backend/src/database/controllers/MatchesController.ts
+++ app/backend/src/database/controllers/MatchesController.ts
@@ -7,18 +7,16 @@ export default class MatchesController {
   getAllMatches = async (req: Request, res: Response) => {
     // const inProgress = req.query.inProgress === 'true';
     const { inProgress } = req.query;
-    if (inProgress === undefined) {
-      const allMatches = await this._service.getAllMatches();
-      return res.status(200).json(allMatches);
+    try {
+      if (inProgress === undefined) {
+        const allMatches = await this._service.getAllMatches();
+        return res.status(200).json(allMatches);
+      }
+      const progress = await this._service.getAllMtachesProgress(inProgress === 'true');
+      return res.status(200).json(progress);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: (error as Error).message });
     }
-    // try {
-
-    const progress = await this._service.getAllMtachesProgress(inProgress === 'true');
-    return res.status(200).json(progress);
-
-    // } catch (error) {
-    //   console.log(error);
-    //   return res.status(500).json({ message: (error as Error).message });
-    // }
   };
 }
