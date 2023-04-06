3d3857da2c96e66db41af8ea923cc8019d538864
Giulliana Rangel <giulliana.rangel@gmail.com>
2023-04-05 22:01:32 -0300

    adding /leaderboard tests

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -4,10 +4,10 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
-import { Model } from 'sequelize';
+import Teams from '../database/models/Teams';
+import Matches from '../database/models/Matches';
 
 import { Response } from 'superagent';
-
 
 chai.use(chaiHttp);
 
@@ -22,7 +22,7 @@ describe('get/leaderboard', () => {
   afterEach(() => {
     sinon.restore();
   })
-  it.only('If returns leaderboard/home list', async () => {
+  it('If returns leaderboard/home list', async () => {
     chaiHttpResponse = await chai.request(app).get('/leaderboard');
     expect(chaiHttpResponse.status).to.be.equal(200);
   })
